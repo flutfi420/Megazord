4a60048dffe0f81479e752526b019a7c5342382e
Camel07 <72527434+Camel07@users.noreply.github.com>
2021-01-23 17:14:36 +0800

    Update gban_db.py

--- ubotindo/modules/no_sql/gban_db.py
+++ ubotindo/modules/no_sql/gban_db.py
@@ -15,90 +15,34 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Global bans utils."""
 
-import threading
+from ubotindo.modules.no_sql import get_collection
 
-
-from ubotindo.modules.no_sql import BASE, SESSION
-
-
-class GloballyBannedUsers(BASE):
-    __tablename__ = "gbans"
-    user_id = Column(Integer, primary_key=True)
-    name = Column(UnicodeText, nullable=False)
-    reason = Column(UnicodeText)
-
-    def __init__(self, user_id, name, reason=None):
-        self.user_id = user_id
-        self.name = name
-        self.reason = reason
-
-    def __repr__(self):
-        return "<GBanned User {} ({})>".format(self.name, self.user_id)
-
-    def to_dict(self):
-        return {"user_id": self.user_id,
-                "name": self.name,
-                "reason": self.reason}
-
-
-class GbanSettings(BASE):
-    __tablename__ = "gban_settings"
-    chat_id = Column(String(14), primary_key=True)
-    setting = Column(Boolean, default=True, nullable=False)
-
-    def __init__(self, chat_id, enabled):
-        self.chat_id = str(chat_id)
-        self.setting = enabled
-
-    def __repr__(self):
-        return "<Gban setting {} ({})>".format(self.chat_id, self.setting)
-
-
-GloballyBannedUsers.__table__.create(checkfirst=True)
-GbanSettings.__table__.create(checkfirst=True)
-
-GBANNED_USERS_LOCK = threading.RLock()
-GBAN_SETTING_LOCK = threading.RLock()
+GBAN_USER = get_collection("GBANS")
+GBAN_SETTINGS = get_collection("GBAN_SETTINGS")
 GBANNED_LIST = set()
 GBANSTAT_LIST = set()
 
 
-def gban_user(user_id, name, reason=None):
-    with GBANNED_USERS_LOCK:
-        user = SESSION.query(GloballyBannedUsers).get(user_id)
-        if not user:
-            user = GloballyBannedUsers(user_id, name, reason)
-        else:
-            user.name = name
-            user.reason = reason
-
-        SESSION.merge(user)
-        SESSION.commit()
-        __load_gbanned_userid_list()
+def gban_user(user_id, name, reason=None) -> None:
+    GBAN_USER.insert_one({
+        '_id': user_id,
+        'name': name,
+        'reason': reason,
+        })
+    __load_gbanned_userid_list()
 
 
-def update_gban_reason(user_id, name, reason=None):
-    with GBANNED_USERS_LOCK:
-        user = SESSION.query(GloballyBannedUsers).get(user_id)
-        if not user:
-            return None
-        old_reason = user.reason
-        user.name = name
-        user.reason = reason
-
-        SESSION.merge(user)
-        SESSION.commit()
-        return old_reason
+def update_gban_reason(user_id, name, reason) -> str:
+    data = GBAN_USER.find_one_and_update(
+        {'_id': user_id},
+        {"$set": {'name': name, 'reason': reason}},
+        upsert=False)
+    return data["reason"]
 
 
-def ungban_user(user_id):
-    with GBANNED_USERS_LOCK:
-        user = SESSION.query(GloballyBannedUsers).get(user_id)
-        if user:
-            SESSION.delete(user)
-
-        SESSION.commit()
-        __load_gbanned_userid_list()
+def ungban_user(user_id) -> None:
+    GBAN_USER.delete_one({'_id': user_id})
+    __load_gbanned_userid_list()
 
 
 def is_user_gbanned(user_id):
@@ -106,76 +50,65 @@ def is_user_gbanned(user_id):
 
 
 def get_gbanned_user(user_id):
-    try:
-        return SESSION.query(GloballyBannedUsers).get(user_id)
-    finally:
-        SESSION.close()
+    return GBAN_USER.find_one({'_id': user_id})
 
 
-def get_gban_list():
-    try:
-        return [x.to_dict() for x in SESSION.query(GloballyBannedUsers).all()]
-    finally:
-        SESSION.close()
+def get_gban_list() -> dict:
+    return [i for i in GBAN_USER.find()]
 
 
-def enable_gbans(chat_id):
-    with GBAN_SETTING_LOCK:
-        chat = SESSION.query(GbanSettings).get(str(chat_id))
-        if not chat:
-            chat = GbanSettings(chat_id, True)
-
-        chat.setting = True
-        SESSION.add(chat)
-        SESSION.commit()
-        if str(chat_id) in GBANSTAT_LIST:
-            GBANSTAT_LIST.remove(str(chat_id))
+def enable_gbans(chat_id) -> None:
+    __gban_setting(chat_id, True)
+    if str(chat_id) in GBANSTAT_LIST:
+        GBANSTAT_LIST.remove(str(chat_id))
 
 
-def disable_gbans(chat_id):
-    with GBAN_SETTING_LOCK:
-        chat = SESSION.query(GbanSettings).get(str(chat_id))
-        if not chat:
-            chat = GbanSettings(chat_id, False)
-
-        chat.setting = False
-        SESSION.add(chat)
-        SESSION.commit()
-        GBANSTAT_LIST.add(str(chat_id))
+def disable_gbans(chat_id) -> None:
+    __gban_setting(chat_id, False)
+    GBANSTAT_LIST.add(str(chat_id))
 
 
-def does_chat_gban(chat_id):
+def __gban_setting(chat_id, setting: bool=True) -> None:
+    if GBAN_SETTINGS.find_one({'_id': chat_id}):
+        GBAN_SETTINGS.update_one(
+            {'_id': chat_id}, {"$set": {'setting': setting}})
+    else:
+        GBAN_SETTINGS.insert_one({'_id': chat_id, 'setting': setting})
+
+
+def does_chat_gban(chat_id) -> bool:
     return str(chat_id) not in GBANSTAT_LIST
 
 
-def num_gbanned_users():
+def num_gbanned_users() -> int:
     return len(GBANNED_LIST)
 
 
-def __load_gbanned_userid_list():
+def __load_gbanned_userid_list() -> None:
     global GBANNED_LIST
-    try:
-        GBANNED_LIST = {x.user_id for x in SESSION.query(GloballyBannedUsers).all()}
-    finally:
-        SESSION.close()
+    GBANNED_LIST = {i["_id"] for i in GBAN_USER.find()}
 
 
-def __load_gban_stat_list():
+def __load_gban_stat_list() -> None:
     global GBANSTAT_LIST
-    try:
-        GBANSTAT_LIST = {x.chat_id for x in SESSION.query(GbanSettings).all() if not x.setting}
-    finally:
-        SESSION.close()
+    GBANSTAT_LIST = {
+        str(i["_id"])
+        for i in GBAN_SETTINGS.find()
+        if not i["setting"]
+    }
 
 
-def migrate_chat(old_chat_id, new_chat_id):
-    with GBAN_SETTING_LOCK:
-        chat = SESSION.query(GbanSettings).get(str(old_chat_id))
-        if chat:
-            chat.chat_id = new_chat_id
-            SESSION.add(chat)
-
-        SESSION.commit()
+def migrate_chat(old_chat_id, new_chat_id) -> None:
+    old = GBAN_SETTINGS.find_one_and_delete({'_id': old_chat_id})
+    if old:
+        setting = old["setting"]
+    else:
+        setting = True
+    GBAN_SETTINGS.update_one(
+        {'_id': new_chat_id},
+        {"$set": {'setting': setting}},
+        upsert=True,
+    )
 
 
 # Create in memory userid to avoid disk access
